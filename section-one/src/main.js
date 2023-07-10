91a5f40e1be7f51ff4538a489e942c9c8be08486
yuqi-wang-forever <1915780288@qq.com>
2023-07-10 23:06:34 +0800

    添加了配置文件，与一些基本操作。

---
+++ section-one/src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hello,ts");
+let first_name = "Jack";
+let last_name = "O'Neill";
+let full_name = `first_name` + `last_name`;
+console.log(full_name);
---
+++ section-one/src/test.ts
@@ -0,0 +1,6 @@
+let test = 'one two'
+let a = 10
+let b = '6'
+let c = 3
+
+// console.log(b / c) // 会抛出错误
